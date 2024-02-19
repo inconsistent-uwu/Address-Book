8d32514be0978e86b8a47d42751c6f31735dbf11
Ankit Sinha Ray <ankitsinharay7921@gmail.com>
2024-02-19 16:16:03 +0530

    Third Commit

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,9 +3,18 @@ import java.util.*;
 public class AddressBookMain {
     public static void main(String[] args) {
 
+        ArrayList<Contacts> contactList = new ArrayList<>();
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Address Book Program");
         Contacts obj = new Contacts();
+
+        System.out.println("Enter the Operation\nAdd Contact :Press 1");
+        int n = sc.nextInt();
+        switch (n) {
+            case 1:
+
+        }
 
     }
 
